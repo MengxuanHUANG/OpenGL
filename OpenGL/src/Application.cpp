4e5cebccd2f8cadcadb7a33a9156fde66d150061
名字 <>
2020-12-24 17:45:51 +0800

    3.0 imgui

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -14,6 +14,10 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+
 int main(void)
 {
 	GLFWwindow* window;
@@ -36,8 +40,18 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
-
 	glfwSwapInterval(1);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+
+	// Setup Platform/Renderer bindings
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
 
 	if (glewInit() != GLEW_OK)
 		std::cout << "Error" << std::endl;
@@ -85,10 +99,10 @@ int main(void)
 		//model, view, projection(MVP) matrix
 		glm::mat4 proj = glm::ortho(0.0f, 1280.0f, 0.0f, 960.0f, -1.0f, 1.0f);
 		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+		//glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
 		//projection * view * model
-		glm::mat4 mvp =  proj * view * model;
+		//glm::mat4 mvp = proj * view * model;
 
 		//shaders
 		Shader shader("res/shaders/Basic.shader");
@@ -96,8 +110,9 @@ int main(void)
 
 		Texture texture("res/texture/ChernoLogo.png");
 		texture.Bind();
+		shader.SetUniform4f("u_Color", 1.0f, 1.0f, 1.0f, 1.0f);
 		shader.SetUniform1i("u_Texture", 0);
-		shader.SetUniformMat4f("u_MVP", mvp);
+		//shader.SetUniformMat4f("u_MVP", mvp);
 
 		//texture.UnBind();
 		va.UnBind();
@@ -109,22 +124,49 @@ int main(void)
 
 		float r = 0.8f;
 		float speed = 0.01f;
+		glm::vec3 translation(0, 0, 0);
+
+		bool show_demo_window = true;
+
 		/* Loop until the user closes the window */
 		while (!glfwWindowShouldClose(window))
 		{
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
 			/* Render here */
 			renderer.Clear();
 
+			glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+
+			//projection * view * model
+			glm::mat4 mvp = proj * view * model;
+
 			shader.Bind();
-			shader.SetUniform4f("u_Color", 1.0f, 1.0f, 1.0f, 1.0f);
+			shader.SetUniformMat4f("u_MVP", mvp);
 
 			renderer.Draw(va, ib, shader);
 
-			r += speed;
-			if (r > 0.99f || r < 0)
+			ImGui::ShowDemoWindow(&show_demo_window);
+
+			// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
 			{
-				speed = -speed;
+				ImGui::Begin("Translation");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::SetWindowSize({ 800.0f, 200.0f });
+				ImGui::SetWindowFontScale(2.0f);
+
+				ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 1280.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
 			}
+
+			// Rendering imgui
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
 			/* Swap front and back buffers */
 			glfwSwapBuffers(window);
 
@@ -132,6 +174,10 @@ int main(void)
 			glfwPollEvents();
 		}
 	}
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
 	glfwTerminate();
 	return 0;
 }
